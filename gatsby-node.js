284c2044e9fdb1bf065e03b2496b3aa5d895b492
Jacob Clarke <jacobclarke718@gail.com>
2020-08-01 22:06:35 -0400

    updated build

--- gatsby-node.js
+++ gatsby-node.js
@@ -68,25 +68,25 @@ exports.onCreateBabelConfig = ({ actions }) => {
   });
 };
 
-const fs = require("fs")
+// const fs = require("fs")
 
-exports.onPreInit = () => {
-  if (process.argv[2] === "build") {
-    fs.rmdirSync(path.join(__dirname, "docs"), { recursive: true })
-    fs.renameSync(
-      path.join(__dirname, "public"),
-      path.join(__dirname, "public_dev")
-    )
-  }
-}
+// exports.onPreInit = () => {
+//   if (process.argv[2] === "build") {
+//     fs.rmdirSync(path.join(__dirname, "docs"), { recursive: true })
+//     fs.renameSync(
+//       path.join(__dirname, "public"),
+//       path.join(__dirname, "public_dev")
+//     )
+//   }
+// }
 
-exports.onPostBuild = () => {
-  fs.renameSync(path.join(__dirname, "public"), path.join(__dirname, "docs"))
-  fs.renameSync(
-    path.join(__dirname, "public_dev"),
-    path.join(__dirname, "public")
-  )
-}
+// exports.onPostBuild = () => {
+//   fs.renameSync(path.join(__dirname, "public"), path.join(__dirname, "docs"))
+//   fs.renameSync(
+//     path.join(__dirname, "public_dev"),
+//     path.join(__dirname, "public")
+//   )
+// }
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
