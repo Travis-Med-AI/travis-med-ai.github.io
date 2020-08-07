a9fb22b5daec69b9c9eb49a2734a5f2e2eeabd00
Jacob Clarke <jacobclarke718@gail.com>
2020-08-07 10:49:42 -0400

    Updates

--- component---src-pages-404-js-e271dbe1928ec66bd0f5.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-e271dbe1928ec66bd0f5.js.map
